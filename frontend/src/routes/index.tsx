dd62f60080f41923df91649fbf3ab6374a06e1d0
Andreas Kamf <andreaskamf@gmail.com>
2025-05-16 12:01:09 +0200

    added useAuth to index.tsx to render based on logged in or not

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,20 +1,45 @@
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { createFileRoute, Link, useRouteContext } from '@tanstack/react-router'
+import { useAuth } from '../hooks/useAuth';
 
-const Home = () => {
-    return (
+const Index = () => {
+  const { user } = useAuth();
+  return (
     <div className="text-center mt-10">
       <h1 className="text-4xl font-bold mb-6">🎲 Welcome to the Game Lounge</h1>
-      <Link
-        to="/games"
-        className="inline-block px-6 py-3 bg-emerald-900 hover:bg-emerald-800 rounded text-white text-lg"
-      >
-        View Games
-      </Link>
+      {!user ? (
+        <Link
+          to="/login"
+          className="inline-block px-6 py-3 bg-emerald-900 hover:bg-emerald-800 rounded text-white text-lg"
+        >
+          Log In
+        </Link>
+      ) : (
+        <div className="flex flex-row justify-center items-center gap-4">
+          <Link
+            to="/games"
+            className="inline-block px-6 py-3 bg-emerald-900 hover:bg-emerald-800 rounded text-white text-lg"
+          >
+            View Games
+          </Link>
+                    <Link
+              to="/profile"
+              className="px-6 py-3 bg-zinc-800 hover:bg-zinc-700 rounded text-white text-lg"
+            >
+              See Profile
+            </Link>
+            {/* <Link
+              to="/high-score"
+              className="px-6 py-3 bg-yellow-700 hover:bg-yellow-600 rounded text-white"
+            >
+              See High Score
+            </Link> */}
+          </div>
+      )}
     </div>
   );
 }
 
 export const Route = createFileRoute('/')({
-  component: Home,
+  component: Index,
 })
 
