9038a702768b9b7ecf5051dc8287687984eea43a
Andreas Kamf <andreaskamf@gmail.com>
2025-05-16 12:52:46 +0200

    added Spring project from Spring initialzr

---
+++ backend/src/main/java/com/example/react_web_project_backend/ReactWebProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.react_web_project_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactWebProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactWebProjectBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/react_web_project_backend/ReactWebProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.react_web_project_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactWebProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
