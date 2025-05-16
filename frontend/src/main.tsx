67aaacf5562bcbd077d30f6ee8782ff195c24a47
Andreas Kamf <andreaskamf@gmail.com>
2025-05-16 15:11:16 +0200

    extracted Provider from main.tsx to context/AuthProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,47 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
-import type { AuthContext, UserDto } from './lib/auth'
+import AuthProvider from './context/AuthProvider'
 import './main.css'
 
-import { createRouter } from '@tanstack/react-router'
-
-const router = createRouter({ routeTree })
-
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
-}
-
-function RootProvider() {
-  const [user, setUser] = React.useState<UserDto | null>(null)
-
-  const login = (user: UserDto) => {
-    setUser(user)
-    localStorage.setItem('user', JSON.stringify(user))
-  }
-
-  const logout = () => {
-    setUser(null)
-    localStorage.removeItem('user')
-  }
-
-  React.useEffect(() => {
-    const stored = localStorage.getItem('user')
-    if (stored) {
-      setUser(JSON.parse(stored))
-    }
-  }, [])
-
-  const context: AuthContext = { user, login, logout }
-
-  return <RouterProvider router={router} context={context} />
-}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RootProvider />
+    <AuthProvider />
   </React.StrictMode>
 )
