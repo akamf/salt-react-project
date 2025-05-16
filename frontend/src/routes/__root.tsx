2ef5026274f3dc1ab79f1d6a8090525f640b4b8a
Andreas Kamf <andreaskamf@gmail.com>
2025-05-16 11:48:25 +0200

    updated header UI

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -11,7 +11,7 @@ const RootComponent = () => {
 
   return (
     <div className="w-full min-h-screen bg-gradient-to-b from-green-600 to-green-900 text-white font-mono">
-      <header className="w-full rounded-b-lg mb-6 text-center bg-emerald-950/50 p-6">
+      <header className="w-full rounded-b-lg mb-6 text-center bg-amber-950/80 p-6">
         <h1 className="text-3xl font-bold">Game Lounge</h1>
         <nav className="mt-4 flex justify-center gap-4">
           <Link to="/" className="hover:underline">Home</Link>
