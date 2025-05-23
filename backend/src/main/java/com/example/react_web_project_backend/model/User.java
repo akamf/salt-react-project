b3abbc1e2488f8ed3d5ccd9a849dca0c4f09905c
Andreas Kamf <andreaskamf@gmail.com>
2025-05-23 10:15:19 +0200

    created User Entity

---
+++ backend/src/main/java/com/example/react_web_project_backend/model/User.java
@@ -0,0 +1,35 @@
+package com.example.react_web_project_backend.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, length = 40)
+    @Size(max = 40)
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String password;
+
+    private String email;
+    private LocalDateTime createdAt;
+    private LocalDateTime lastLogin;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
