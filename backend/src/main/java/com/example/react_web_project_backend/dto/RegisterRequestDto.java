bd0b416e154ef3376a047439991f1c4511ff57fe
Andreas Kamf <andreaskamf@gmail.com>
2025-05-23 11:02:50 +0200

    updated registerdto

--- backend/src/main/java/com/example/react_web_project_backend/dto/RegisterRequestDto.java
+++ backend/src/main/java/com/example/react_web_project_backend/dto/RegisterRequestDto.java
@@ -1,11 +1,16 @@
 package com.example.react_web_project_backend.dto;
 
+import com.example.react_web_project_backend.model.User;
 import jakarta.validation.constraints.NotBlank;
 
 public record RegisterRequestDto(
         @NotBlank(message = "Username cannot be blank")
-        String name,
+        String username,
 
         @NotBlank(message = "Password cannot be blank")
         String password
-) {}
+) {
+        public static User userFromDto(RegisterRequestDto dto) {
+                return new User(dto.username(), dto.password());
+        }
+}
